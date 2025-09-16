a677bef02d9f8c0e628dba65a04389ad43f8cec1
mahenq <90361623+mahenq@users.noreply.github.com>
2025-09-16 11:47:26 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["images.pexels.com"],
+    // atau lebih fleksibel:
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "images.pexels.com",
+    //     pathname: "/photos/**",
+    //   },
+    // ],
+  },
 };
 
 export default nextConfig;
